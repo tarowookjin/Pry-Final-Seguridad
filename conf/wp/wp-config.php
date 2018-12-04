fa4da16450a14b158342d1c241920dd372e68e78
Taro <tarowj05@gmail.com>
2018-12-03 20:19:28 -0500

    Add Vitual HOST

---
+++ conf/wp/wp-config.php
@@ -0,0 +1,20 @@
+<?php
+
+include '.env.php';
+$table_prefix  = 'wp_';
+/** WordPress absolute path to the Wordpress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+//Begin Really Simple SSL Load balancing fix
+$server_opts = array("HTTP_CLOUDFRONT_FORWARDED_PROTO" => "https", "HTTP_CF_VISITOR"=>"https", "HTTP_X_FORWARDED_PROTO"=>"https", "HTTP_X_FORWARDED_SSL"=>"on", "HTTP_X_PROTO"=>"SSL", "HTTP_X_FORWARDED_SSL"=>"1");
+foreach( $server_opts as $option => $value ) {
+ if ((isset($_ENV["HTTPS"]) && ( "on" == $_ENV["HTTPS"] )) || (isset( $_SERVER[ $option ] ) && ( strpos( $_SERVER[ $option ], $value ) !== false )) ) {
+   $_SERVER[ "HTTPS" ] = "on";
+     break;
+      }
+      }
+//      //END Really Simple SSL 
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
